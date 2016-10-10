b4e47756b35c773e3233c7cba86b9e2f88c9fac6
weirdvector <weirdvector@gmail.com>
2016-10-10 18:04:10 -0400

    Adds pagination and styling to topics

--- app/public/js/controllers/alltopicscontroller.js
+++ app/public/js/controllers/alltopicscontroller.js
@@ -1,6 +1,7 @@
 laddrControllers.controller('AllTopicsController', ['$scope', '$location', '$http', '$sessionStorage', function($scope, $location, $http, $sessionStorage) {
   $scope.$storage = $sessionStorage;
   $scope.topics = [];
+  $scope.page = 0;
 
   if ($scope.$storage.ldrToken != undefined) {
     $http
@@ -11,7 +12,11 @@ laddrControllers.controller('AllTopicsController', ['$scope', '$location', '$htt
       })
       .success(function(data, status, headers, config) {
 
-        $scope.topics = data;
+        // pagination
+        tpp = 10; //topics per page ;)
+        for (i = 0; i < data.length; i += tpp) {
+          $scope.topics.push(data.slice(i, i + tpp));
+        }
 
         //show logout
         $scope.logout = true;
@@ -22,5 +27,19 @@ laddrControllers.controller('AllTopicsController', ['$scope', '$location', '$htt
       });
   } else {
     $location.url('/login');
+  }
+
+  $scope.nextPage = function() {
+    $scope.page++;
+    if ($scope.page >= $scope.postings.length) {
+      $scope.page = $scope.postings.length - 1;
+    }
+  }
+
+  $scope.prevPage = function() {
+    $scope.page--;
+    if ($scope.page < 0) {
+      $scope.page = 0;
+    }
   }
 }]);
