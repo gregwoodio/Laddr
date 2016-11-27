e20181a73e55a73e30674db6e3f41643da24c0db
weirdvector <weirdvector@gmail.com>
2016-11-27 13:42:55 -0500

    FCM test

--- app/api/applicants.js
+++ app/api/applicants.js
@@ -7,37 +7,37 @@ module.exports = function(app, models) {
   app.get('/api/applicants', mw.verifyToken, function(req, res) {
 
     models.Application.findAll({
-        include: [{
-          model: models.Posting,
-          where: {
-            ProfileID: req.decoded.ProfileID
-          }
-        }, {
-          model: models.Profile,
-          include: [{
-            model: models.User
-          }]
-          // where: {
-          //   ProfileID:  
-          // }
-        }]
-      })
-      .then(function(applications) {
-        for (i = 0; i < applications.length; i++) {
-          applications[i].LdrProfile.Password = undefined;
+      include: [{
+        model: models.Posting,
+        where: {
+          ProfileID: req.decoded.ProfileID
         }
+      }, {
+        model: models.Profile,
+        include: [{
+          model: models.User
+        }]
+        // where: {
+        //   ProfileID:  
+        // }
+      }]
+    })
+    .then(function(applications) {
+      for (i = 0; i < applications.length; i++) {
+        applications[i].LdrProfile.Password = undefined;
+      }
 
-        res.json({
-          success: true,
-          applications: applications
-        });
-      })
-      .catch(function(err) {
-        res.status(500).json({
-          success: false,
-          message: err.message
-        });
+      res.json({
+        success: true,
+        applications: applications
       });
+    })
+    .catch(function(err) {
+      res.status(500).json({
+        success: false,
+        message: err.message
+      });
+    });
 
   });
 
