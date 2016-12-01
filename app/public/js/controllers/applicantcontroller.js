25d702cd62894ecbda9d32a2a5584aae15679480
PETERPHAN\Peter <peter-phan@live.com>
2016-12-01 12:42:12 -0500

    Ignore these changes, greg already fixed it

--- app/public/js/controllers/applicantcontroller.js
+++ app/public/js/controllers/applicantcontroller.js
@@ -2,7 +2,11 @@
 
 laddrControllers.controller('ApplicantController', ['$scope', '$http', '$location', 'LoginService',
   function($scope, $http, $location, LoginService) {
-
+      
+  /* Peter's NG-Modal Popup */
+  
+  /* End Here for Modal */      
+      
   $scope.applications = {};
 
   $scope.updateApplication = function(newValue, profileID, postingID) {
@@ -51,4 +55,62 @@ laddrControllers.controller('ApplicantController', ['$scope', '$http', '$locatio
 
 }]);
 
+/* Modal Open/Close */
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('ModalDemoCtrl', function($uibModal, $log, $document) {
+  var $ctrl = this;
+  $ctrl.items = ['item1', 'item2', 'item3'];
 
+  $ctrl.animationsEnabled = true;
+
+  $ctrl.open = function(size, parentSelector) {
+    var parentElem = parentSelector ?
+      angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      controllerAs: '$ctrl',
+      size: size,
+      appendTo: parentElem,
+      resolve: {
+        items: function() {
+          return $ctrl.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function(selectedItem) {
+      $ctrl.selected = selectedItem;
+    }, function() {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+
+
+  $ctrl.toggleAnimation = function() {
+    $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+  };
+});
+
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+
+angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function($uibModalInstance, items) {
+  var $ctrl = this;
+  $ctrl.items = items;
+  $ctrl.selected = {
+    item: $ctrl.items[0]
+  };
+
+  $ctrl.ok = function() {
+    $uibModalInstance.close($ctrl.selected.item);
+  };
+
+  $ctrl.cancel = function() {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
